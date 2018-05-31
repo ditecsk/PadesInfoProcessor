fdcc8ec80ec7ab86f8043b322a77d1382a773883
pavlikditec <pavlik@ditec.sk>
2018-05-31 15:05:57 +0200

    signing removed

--- PadesInfoProcessor/Properties/AssemblyInfo.cs
+++ PadesInfoProcessor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PadesInfoProcessorCMD")]
+[assembly: AssemblyTitle("PadesInfoProcessor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PadesInfoProcessorCMD")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("DITEC, a.s.")]
+[assembly: AssemblyProduct("PadesInfoProcessor")]
+[assembly: AssemblyCopyright("Copyright © DITEC, a.s. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
